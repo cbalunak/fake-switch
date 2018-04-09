acc0e430dd4c3298bf0d242523afba07490fad07
cbalunak <cbalunak@yahoo.com>
2018-04-09 23:06:54 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const Service, Characteristic;
 module.exports = function (homebridge) {
   Service = homebridge.hap.Service;
   Characteristic = homebridge.hap.Characteristic;
-  homebridge.registerAccessory("fake-switch", "MyAwesomeSwitch", mySwitch);
+  homebridge.registerAccessory("fake-switch", "fake-switch", mySwitch);
 };
 
 mySwitch.prototype = {
